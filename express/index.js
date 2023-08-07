baf31718b90657ae8c4c5ec6d7626dc4c1a8a3a1
0x1m40 <chuntdevnx@gmail.com>
2023-08-07 21:55:19 +0700

    update html

--- express/index.js
+++ express/index.js
@@ -24,4 +24,6 @@ app.use((req, res) => {
     res.sendFile('/views/404.html', {root: __dirname});
 });
 
-app.listen(3000);
+app.listen(3000, ()=> {
+    console.log('listening for requests on port 3000\nhttp://localhost:3000' );
+});
