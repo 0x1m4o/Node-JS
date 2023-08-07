80f554d825b827a28a77b23adad1b99172b4d219
0x1m40 <chuntdevnx@gmail.com>
2023-08-07 21:52:08 +0700

    learn express route

---
+++ express/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log(req.url);
+
+    res.sendFile('./views/index.html', {root: __dirname})
+})
+
+app.get('/about', (req, res) => {
+    console.log(req.url);
+
+    res.sendFile('./views/about.html', {root: __dirname})
+})
+
+app.get('/about-us', (req, res) => {
+    console.log(req.url);
+
+    res.redirect('/about');
+})
+
+app.use((req, res) => {
+    res.sendFile('/views/404.html', {root: __dirname});
+});
+
+app.listen(3000);
